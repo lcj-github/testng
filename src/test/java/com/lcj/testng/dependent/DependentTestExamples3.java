48aba51fe0d13b33b39bc58e34a2abdd44eda5da
李 春建 <lichunjian@gw.com.cn>
2017-08-29 16:11:26 +0800

    first commit

---
+++ src/test/java/com/lcj/testng/dependent/DependentTestExamples3.java
@@ -0,0 +1,22 @@
+package com.lcj.testng.dependent;
+
+import org.testng.annotations.Test;
+
+public class DependentTestExamples3 {
+	
+	 	@Test(dependsOnGroups = { "test-group" })
+	    public void groupTestOne() {
+	        System.out.println("Group Test method one");
+	    }
+	 
+	    @Test(groups = { "test-group" })
+	    public void groupTestTwo() {
+	        System.out.println("Group test method two");
+	    }
+	 
+	    @Test(groups = { "test-group" })
+	    public void groupTestThree() {
+	        System.out.println("Group Test method three");
+	    }
+
+}
